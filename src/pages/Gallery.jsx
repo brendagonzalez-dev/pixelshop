f566805eba250472fe26d08b00175192937c2b41
brenda <brenda.gonzalez.dev@gmail.com>
2023-09-13 22:39:49 -0600

    [ADD]: get gallery data and photo detail

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,104 +1,56 @@
 import {Link} from 'react-router-dom'
 import {useDispatch} from 'react-redux'
 import {setIsLoading} from '../store/slices/isLoading.slice'
-import {useEffect} from 'react'
+import {useEffect, useState} from 'react'
+import {db} from '../../firebase-app/firebase.js'
+import {collection, getDocs} from "firebase/firestore"; 
 
 const Gallery = () => {
     
     const dispatch = useDispatch()
+    const [photos, setPhotos] = useState([])
 
     useEffect(() => {
-        dispatch(setIsLoading(true))
-
-        setTimeout(() => {
-            dispatch(setIsLoading(false)) 
-        }, 1500);
+        getData()
     }, [])
+
+    const getData = async () => {
+        dispatch(setIsLoading(true))
+        
+        const aux = []
+        const querySnapshot = await getDocs(collection(db, "photos"))
+
+        querySnapshot.forEach((doc) => {
+            const data = doc.data()
+            data.id = doc.id
+            
+            console.log(data)
+            aux.push(data)
+        })
+        
+
+        setPhotos(aux.slice(0, 10))
+        dispatch(setIsLoading(false)) 
+    }
+
+    
 
     return(
         <main className="container">
             <h1 className="main__title">Galeria</h1>
             <section className="section gallery">
-                <article className="gallery__item">
-                    <Link to={`/gallery/detail/666`}>
-                        <img src="https://picsum.photos/400/600" alt="image description" />
-                        <div className="gallery__item-overlay">
-                            <button className="principal__button">Ver Detalle</button>
-                        </div>
-                    </Link>
-                </article>
-                <article className="gallery__item">
-                    <Link to={`/gallery/detail/666`}>
-                        <img src="https://picsum.photos/400/600" alt="image description" />
-                        <div className="gallery__item-overlay">
-                            <button className="principal__button">Ver Detalle</button>
-                        </div>
-                    </Link>
-                </article>
-                <article className="gallery__item">
-                    <Link to={`/gallery/detail/666`}>
-                        <img src="https://picsum.photos/400/600" alt="image description" />
-                        <div className="gallery__item-overlay">
-                            <button className="principal__button">Ver Detalle</button>
-                        </div>
-                    </Link>
-                </article>
-                <article className="gallery__item">
-                    <Link to={`/gallery/detail/666`}>
-                        <img src="https://picsum.photos/400/600" alt="image description" />
-                        <div className="gallery__item-overlay">
-                            <button className="principal__button">Ver Detalle</button>
-                        </div>
-                    </Link>
-                </article>
-                <article className="gallery__item">
-                    <Link to={`/gallery/detail/666`}>
-                        <img src="https://picsum.photos/400/600" alt="image description" />
-                        <div className="gallery__item-overlay">
-                            <button className="principal__button">Ver Detalle</button>
-                        </div>
-                    </Link>
-                </article>
-                <article className="gallery__item">
-                    <Link to={`/gallery/detail/666`}>
-                        <img src="https://picsum.photos/400/600" alt="image description" />
-                        <div className="gallery__item-overlay">
-                            <button className="principal__button">Ver Detalle</button>
-                        </div>
-                    </Link>
-                </article>
-                <article className="gallery__item">
-                    <Link to={`/gallery/detail/666`}>
-                        <img src="https://picsum.photos/400/600" alt="image description" />
-                        <div className="gallery__item-overlay">
-                            <button className="principal__button">Ver Detalle</button>
-                        </div>
-                    </Link>
-                </article>
-                <article className="gallery__item">
-                    <Link to={`/gallery/detail/666`}>
-                        <img src="https://picsum.photos/400/600" alt="image description" />
-                        <div className="gallery__item-overlay">
-                            <button className="principal__button">Ver Detalle</button>
-                        </div>
-                    </Link>
-                </article>
-                <article className="gallery__item">
-                    <Link to={`/gallery/detail/666`}>
-                        <img src="https://picsum.photos/400/600" alt="" />
-                        <div className="gallery__item-overlay">
-                            <button className="principal__button">Ver Detalle</button>
-                        </div>
-                    </Link>
-                </article>
-                <article className="gallery__item">
-                    <Link to={`/gallery/detail/666`}>
-                        <img src="https://picsum.photos/400/600" alt="" />
-                        <div className="gallery__item-overlay">
-                            <button className="principal__button">Ver Detalle</button>
-                        </div>
-                    </Link>
-                </article>
+                {
+                    photos?.map(item => (
+                        <article className="gallery__item" key={item.id}>
+                            <Link to={`/gallery/detail/${item.id}`}>
+                                <img src={item.url} alt="image description" />
+                                <div className="gallery__item-overlay">
+                                    <button className="principal__button">Ver Detalle</button>
+                                </div>
+                            </Link>
+                        </article>
+                    ))
+                }
             </section>
             
         </main>
--- src/pages/PhotoDetail.jsx
+++ src/pages/PhotoDetail.jsx
@@ -2,23 +2,42 @@ import Overlay from '../components/Overlay'
 import {useState, useEffect} from 'react'
 import {useDispatch} from 'react-redux'
 import {setIsLoading} from '../store/slices/isLoading.slice'
-import PurchaseForm from '../components/forms/PurchaseForm'
+//import PurchaseForm from '../components/forms/PurchaseForm'
 //import EditPhotoForm from '../components/forms/EditPhotoForm'
 //import ModalOptions from '../components/commons/ModalOptions'
-
+import {db} from '../../firebase-app/firebase.js'
+import {getDoc, doc} from "firebase/firestore"
+import {useParams} from 'react-router-dom'
 
 const PhotoDetail = () => {
-    const [isVisible, setIsVisible] = useState(false)
+
     const dispatch = useDispatch()
+    const [isVisible, setIsVisible] = useState(false)
+    const [detail, setDetail] = useState({})
+    const {id} = useParams()
 
     useEffect(() => {
+        getData(id)
+    }, [id])
+
+    const getData = async id => {
         dispatch(setIsLoading(true))
-
-        setTimeout(() => {
-            dispatch(setIsLoading(false)) 
-        }, 1500);
-    }, [])
-
+        
+        try {
+            const docRef = await doc(db, "photos", id)
+            const docSnap = await getDoc(docRef)
+            if(docSnap.exists()){
+                setDetail(docSnap.data())
+            }else{
+                //redireccionar a vista 404
+    
+            }
+            dispatch(setIsLoading(false))
+            
+        } catch (error) {
+            console.log(error)
+        }
+    }
 
     const handlePurchase = () => {
         setIsVisible(true)
@@ -28,22 +47,22 @@ const PhotoDetail = () => {
         <main className="container">
             <section className="section photo-detail-wrapper">
                 <article className="photo-detail__image">
-                    <img src="https://picsum.photos/400/500" alt="" />
+                    <img src={detail.url} alt="" />
                 </article>
                 <article className="photo-detail__information">
-                    <p className="gb-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam alias dicta ea et nesciunt iure deleniti repudiandae laboriosam corporis amet.</p>
+                    <p className="gb-2">{detail.description}</p>
                     <p className="gb-1">
                         <i className='bx-fw bx bx-camera' ></i>
-                        Autor: Pepe Perez
+                        Autor: {detail.author}
                     </p>
-                    {/* photo-detail__stock--sold-out */}
-                    <p className="photo-detail__stock gb-1">
+                    <p 
+                    className={`photo-detail__stock gb-1 ${detail.stock === 0 && "photo-detail__stock--sold-out"} ${ (detail.stock >= 1 && detail.stock <= 10)&& "photo-detail__stock--warn"}`}>
                         <i className='bx-fw bx bx-coin-stack' ></i>
-                        Stock: 100
+                        Stock: {detail.stock}
                     </p>
                     <p className="gb-3">
                         <i className='bx-fw bx bx-purchase-tag-alt' ></i>
-                        Precio: $4500
+                        Precio: ${detail.cost} MXN
                     </p>
                     <p className="gb-2">
                         <i className='bx-fw bx bx-package'></i>
